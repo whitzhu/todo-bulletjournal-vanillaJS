0b87c5d9f72e80f470894740b2adefce22c9a43b
whitzhu <whitzhu@gmail.com>
2017-06-22 15:42:33 -0700

    [Favicon] add favicon.ico, and server middleware to serve file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
+
 const app = express();
 
+app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
